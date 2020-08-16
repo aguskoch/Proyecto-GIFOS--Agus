937776c7437b3732088851db9e1145fc76d25924
Agustina koch <agustinapkoch@gmail.com>
2020-08-16 12:08:56 -0300

    Favorites tab

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+// // Armado de Tabs
+// let tablinks = document.getElementsByClassName ("tablinks");
+// tablinks.addEventListener ("click", changeTabs);
+
+
+// function changeTabs (evt, tabname) {
+//     let tabContent = document.getElementsByClassName ("tab");
+//     let i = 0; 
+
+//     for (i = 0; i < tabContent.length; i++){
+//         tabContent[i].style.display = "none";
+//     }
+
+//     for (i = 0; i < tablinks.length; i++){
+//         tablinks[i].className = tablinks[i].className.replace ("active", " ");
+//     }
+
+//     document.getElementById(tabname).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
