2597df72ffba84e74689d0f4afbd175d1edd75bd
Agustina koch <agustinapkoch@gmail.com>
2020-08-29 16:59:01 -0300

    Dark and Light Toggle terminado

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,21 @@
-// // Armado de Tabs
-// let tablinks = document.getElementsByClassName ("tablinks");
-// tablinks.addEventListener ("click", changeTabs);
+//*************************Apply toggle between Dark and Light Mode****************************************
+const btnSwitch = document.querySelector (".switch");
+btnSwitch.addEventListener ("click", functionDarkMode);
 
+function functionDarkMode (){
+    document.body.classList.toggle ("dark");
 
-// function changeTabs (evt, tabname) {
-//     let tabContent = document.getElementsByClassName ("tab");
-//     let i = 0; 
+    //Saving the mode on LocalStorage
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark-mode", "true");
+    } else{
+        localStorage.setItem("dark-mode", "false");
+    }
+}
 
-//     for (i = 0; i < tabContent.length; i++){
-//         tabContent[i].style.display = "none";
-//     }
-
-//     for (i = 0; i < tablinks.length; i++){
-//         tablinks[i].className = tablinks[i].className.replace ("active", " ");
-//     }
-
-//     document.getElementById(tabname).style.display = "block";
-//     evt.currentTarget.className += " active";
-// }
+//We obtain the mode that the User has - light or dark
+if(localStorage.getItem("dark-mode") === "true"){
+    document.body.classList.add ("dark");
+} else {
+    document.body.classList.remove ("dark");
+}
